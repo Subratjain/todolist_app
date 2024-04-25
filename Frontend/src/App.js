5b3bbc6bdff2ce5c618acf91e057b029ac3e07d6
Subratjain <103668727+Subratjain@users.noreply.github.com>
2024-04-26 01:13:09 +0530

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Homepage from './Components/Homepage';
 
 function App() {
   return (
-    <>
     <Homepage/>
-    {/* Ramshyam */}
-    </>
   );
 }
 
